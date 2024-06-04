264e66b5a9adb1258b633615af333d5277bdb215
DavidIvan025 <davidivansan@hotmail.com>
2024-06-03 20:26:52 -0600

    fix: get and create members function fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,8 +3,6 @@ require('dotenv').config();
 const app = require('./app');
 require('./database');
 
-main().catch(err => console.log(err));
-
 async function main() {
     await app.listen(app.get('port'));
     console.log('Server is running on port: ', app.get('port'));
