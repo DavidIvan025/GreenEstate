d9ca95530a61dfc660fb4cd3b853c553cb335437
DavidIvan025 <davidivansan@hotmail.com>
2024-07-11 21:39:48 -0600

    feat: home section and pretty plugin updated for only jsx files

--- frontend/src/components/ui/SpinnerLoading.jsx
+++ frontend/src/components/ui/SpinnerLoading.jsx
@@ -1,14 +1,7 @@
 export default function SpinnerLoading() {
   return (
-<<<<<<< HEAD
     <div className="loadingSpinnerContainer">
       <div className="loadingSpinner"></div>
     </div>
   );
-=======
-    <div className="loadingSpinnerContainer"> 
-        <div className="loadingSpinner"></div>
-    </div>
-  )
->>>>>>> 1c325eb56a95b56aca6e16e74e09b141bce11010
 }
