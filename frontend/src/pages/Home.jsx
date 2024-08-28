3d67a837b04060832f8cac87a5afca0e9abf3b1b
DavidIvan025 <davidivansan@hotmail.com>
2024-08-27 22:58:13 -0600

    feat: add blogPost cms layout and articles section is now a component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,6 +6,7 @@ import LuxurySlideShow from "../components/LuxurySlideShow";
 import EmailForm from "../components/EmailForm";
 import FAQ from "../components/FAQ";
 import Testimonials from "../components/Testimonials";
+import Articles from "../components/Articles";
 
 export default function Home() {
   return (
@@ -212,34 +213,7 @@ export default function Home() {
       </section>
 
       <section>
-        <div className="flex flex-col pb-12 lg:flex-row">
-          <h2 className="mb-4 grow font-bold">Read our latest articles</h2>
-          <Button className="min-w-full md:min-w-56">
-            Browse all articles
-          </Button>
-        </div>
-        <div className="article grid grid-flow-row grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-3">
-          <div className="article-Card relative min-h-[450px] w-full rounded-2xl border border-gray shadow-sm">
-            <img
-              className="max-w-full rounded-t-2xl object-cover object-center"
-              src="./images/img.jpg"
-              alt="article_id"
-            />
-            <div className="mb-6 space-y-4 p-6">
-              <div className="flex space-x-1">
-                <p className="text-fs-sm">March 5, 2024 •</p>
-                <p className="text-fs-sm">5 min read</p>
-              </div>
-              <h5 className="font-bold">
-                Turning Dreams into Achievable Targets
-              </h5>
-              <p className="text-fs-base">
-                Discuss how virtual reality is revolutionizing the way potential
-                buyers explore
-              </p>
-            </div>
-          </div>
-        </div>
+        <Articles />
       </section>
 
       <section className="full-width z-20 bg-[#F7FCF9]">
